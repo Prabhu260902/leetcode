8f5c4010a65a7cb20510b33430577950cba3b658
Prabhu <84372886+Prabhu260902@users.noreply.github.com>
2022-09-06 00:06:19 +0530

    Time: 52 ms (25.57%), Space: 45.7 MB (32.80%) - LeetHub

---
+++ 322-coin-change/322-coin-change.java
@@ -0,0 +1,25 @@
+class Solution {
+    static int minCoins(int n,int[] a,int[] dp)
+    {
+        if(n==0) return 0;
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<a.length;i++)
+        {
+            if(n-a[i]>=0){
+                int subAns=0;
+                if(dp[n-a[i]]!=-1) subAns=dp[n-a[i]];
+                else subAns=minCoins(n-a[i],a,dp);
+                if(subAns!=Integer.MAX_VALUE && subAns+1<ans) ans=subAns+1;
+            }
+        }
+        return dp[n]=ans;
+    }
+    public int coinChange(int[] coins, int amount) {
+        int dp[]=new int[amount+1];
+        Arrays.fill(dp,-1);
+        dp[0]=0;
+        int ans=minCoins(amount,coins,dp);
+        if(ans>amount) return -1;
+        return ans;
+    }
+}
