6a58f263d238fb7fd11ffc9d53779c698a10e557
Prabhu <84372886+Prabhu260902@users.noreply.github.com>
2022-09-24 14:51:43 +0530

    Time: 103 ms (26.54%), Space: 43.4 MB (15.01%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
-    bool sol(vector<int>&dp,vector<int>&arr,int s){
+    bool sol(vector<bool>&dp,vector<int>&arr,int s){
+        
         if(s<0 or s>=arr.size()) return false;
         if(arr[s]==0) return true;
-        if(dp[s]!=0) return false;
-        dp[s]=1;
-        bool l=sol(dp,arr,s-arr[s]);
-        bool r=sol(dp,arr,s+arr[s]);
-        return l||r;
+        if(dp[s]!=false) return false;
+        dp[s]=true;
+        bool l=sol(dp,arr,s+arr[s]);
+        bool r=sol(dp,arr,s-arr[s]);
+        return dp[s]=l or r;
     }
     bool canReach(vector<int>& arr, int start) {
-        vector<int>dp(arr.size(),0);
+        vector<bool>dp(arr.size()+1,false);
         bool flag=false;
         for(auto i:arr) if(i==0) flag=true;
         if(flag) return sol(dp,arr,start);
-        else return false;
+        return flag;
     }
 };
