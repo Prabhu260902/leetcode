7a4df25a3756889c8482e6cac48df97d11e8ec3c
Prabhu <84372886+Prabhu260902@users.noreply.github.com>
2022-09-24 06:44:03 +0530

    Time: 2 ms (59.23%), Space: 7.7 MB (77.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -3,9 +3,8 @@ public:
     int ans(vector<int>&dp,vector<int>&nums,int ind){
         if(ind>=nums.size()) return 0;
         if(dp[ind]!=-1) return dp[ind];
-        int left=nums[ind]+ans(dp,nums,ind+2);
-        int right=ans(dp,nums,ind+1);
-        dp[ind]=max(left,right);
+        
+        dp[ind]=max(nums[ind]+ans(dp,nums,ind+2),ans(dp,nums,ind+1));
         return dp[ind];
     }
     int rob(vector<int>& nums) {
