1e972368d09282f5ab058eec28bde56b43ab8e40
Prabhu <84372886+Prabhu260902@users.noreply.github.com>
2022-11-18 11:54:36 +0530

    Added solution - LeetHub

---
+++ Convert String to LowerCase - GFG/convert-string-to-lower-case.java
@@ -0,0 +1,31 @@
+//{ Driver Code Starts
+// Initial template for Java
+
+import java.util.*;
+import java.io.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            String S = read.readLine();
+            Solution ob = new Solution();
+
+            System.out.println(ob.toLower(S));
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function template for Java
+
+class Solution {
+    static String toLower(String S) {
+        // code here
+        S=S.toLowerCase();
+        return S;
+    }
+}
