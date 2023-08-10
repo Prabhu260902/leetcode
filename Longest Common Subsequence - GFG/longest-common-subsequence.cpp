17a31c4294c1e60f51e24b9b9ffcf9bca5a45000
Prabhu <84372886+Prabhu260902@users.noreply.github.com>
2023-08-10 10:38:46 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -1,43 +1,46 @@
-// { Driver Code Starts
+//{ Driver Code Starts
 #include<bits/stdc++.h>
 const int mod=1e9+7;
 using namespace std;
 
- // } Driver Code Ends
+// } Driver Code Ends
 // function to find longest common subsequence
 
 class Solution
 {
     public:
     //Function to find the length of longest common subsequence in two strings.
-    int solution(string s1, string s2, int x, int y,vector<vector<int>>&dp){
-        if(x<0 || y<0) return 0;
-        if(dp[x][y]!=-1) return dp[x][y];
-        if(s1[x]==s2[y]) return dp[x][y] = 1+solution(s1,s2,x-1,y-1,dp);
-        else return dp[x][y] = max(solution(s1,s2,x-1,y,dp),solution(s1,s2,x,y-1,dp));
+    int solve(int ind1,int ind2,string &s1,string& s2,vector<vector<int>>&dp){
+        if(ind1 < 0 || ind2 < 0) return 0;
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        if(s1[ind1] == s2[ind2]) return dp[ind1][ind2] = 1 + solve(ind1-1,ind2-1,s1,s2,dp);
+        else{
+            return dp[ind1][ind2] = max(solve(ind1-1,ind2,s1,s2,dp),solve(ind1,ind2-1,s1,s2,dp));
+        }
     }
-    int lcs(int x, int y, string s1, string s2)
+    int lcs(int n, int m, string s1, string s2)
     {
         // your code here
-        vector<vector<int>>dp(x,vector<int>(y,-1));
-        return solution(s1,s2,x-1,y-1,dp);
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        return solve(n-1,m-1,s1,s2,dp);
     }
 };
 
 
-// { Driver Code Starts.
+//{ Driver Code Starts.
 int main()
 {
-    int t,n,k,x,y;
+    int t,n,m;
     cin>>t;
     while(t--)
     {
-        cin>>x>>y;          // Take size of both the strings as input
+        cin>>n>>m;          // Take size of both the strings as input
         string s1,s2;
         cin>>s1>>s2;        // Take both the string as input
         Solution ob;
-        cout << ob.lcs(x, y, s1, s2) << endl;
+        cout << ob.lcs(n, m, s1, s2) << endl;
     }
     return 0;
 }
-  // } Driver Code Ends
+
+// } Driver Code Ends
